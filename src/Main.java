8cd99808db71691fdbaad70438588b5d52ad6e65
Gavin <gvnmcg517@gmail.com>
2019-04-09 21:06:01 -0600

    renamed GDT

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,198 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-//import javafx.scene.layout.GridPaneBuilder;
-
-/**
- * The Main Class of Mobile Agents
- */
 public class Main extends Application {
 
-    public static boolean debugMessaging = false;
-    public static boolean debugMobileAgents = false;
-    public static boolean debugGraphNode = false;
-    String fileSelction = "sample";
+    private String fileSelectionStr = "sample";
+    private File fileSelection;
 
+    public static final int WIDTH = 700;
+    public static final int HEIGHT = 500;
 
-    /**
-     * Start the basic components
-     * - The graph data stucture
-     * - The display for the graph
-     * @param primaryStage
-     * @throws Exception
-     */
+    private GraphDisplay graphDisplay;
+
+    private Stage window;
+
+    private String buttonStyle =  "    -fx-text-fill: #101024;\n" +
+            "    -fx-background-color: #EEEEEE;\n" +
+            "    -fx-border-color: #111111;" +
+            "    -fx-padding: 8;";
+
+    private Text selectedFileText;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        // Initialize gragh data structure
-        Graph graph = new Graph(new File("resources/Star"));
+        window = primaryStage;
 
-        // Display graph
-        GraphDisplay graphDisplay = new GraphDisplay(graph);
-
-        MobileAgent test = new MobileAgent(graph.getStation(), graphDisplay,  true);
-        // Start simulation
-        graph.startThreads();
-
-
-        // Does not work???
+        //ends all threads and closes window
         primaryStage.setOnCloseRequest(e -> {
             System.exit(0);
             primaryStage.close();
         });
 
-
-        primaryStage.setScene(new Scene(graphDisplay.getRoot(), 700, 500));
+        primaryStage.setTitle("Mobile Agents");
+        primaryStage.setScene(introScene());
         primaryStage.show();
-
     }
 
 
+    private Graph initGraph(File file){
+        // Initialize gragh data structure
+        Graph graph = new Graph(file);
+
+        // Display graph
+        graphDisplay = new GraphDisplay(graph);
+
+        new MobileAgent(graph.getStation(), graphDisplay,  true);
+        // Start simulation
+        graph.startThreads();
+
+        return graph;
+    }
+
+    private Scene introScene(){
+
+        //root to this scene
+        GridPane introRoot = new GridPane();
+
+
+        Text title = new Text("Mobile Agents");
+        title.setStyle("-fx-font: 24 arial;");
+
+        //Button that confirms the selection and starts the simulation
+        Button startButton = new Button("Start");
+        startButton.setStyle(buttonStyle);
+        startButton.addEventHandler(MouseEvent.MOUSE_CLICKED, handleConfirm());
+
+        //pane containing all the graph options
+        ScrollPane graphselectionPane = graphSelectionScrollPane();
+
+        //opens a dialog giving infor mationa about the project
+        Button infoBoxButton = new Button("Info");
+        infoBoxButton.setStyle(buttonStyle);
+        infoBoxButton.setOnAction(event -> {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+
+            alert.setGraphic(null);
+            alert.setTitle("");
+            alert.setHeaderText("What is Mobile Agents?");
+            alert.setContentText("It is a simulation of a communication network as it launches concurrent programs " +
+                    "to contain a 'forest fire' destroying the nodes on the network.");
+
+//            alert.setOnCloseRequest(event -> alert.close());
+            alert.showAndWait();
+            alert.close();
+        });
+
+        //tells what file is selected byt he selector
+        selectedFileText = new Text(fileSelectionStr);
+
+        //button that opens a file selector
+        Button openButton = new Button("Open");
+        openButton.setStyle(buttonStyle);
+        openButton.setOnAction(event -> {
+
+            FileChooser fileChooser = new FileChooser();
+//            fileChooser.setInitialDirectory(new File("resources"));
+
+            try {
+//                fileSelectionStr = fileChooser.showOpenDialog(window).getName();
+                fileSelection = fileChooser.showOpenDialog(window);
+
+            }catch (NullPointerException e){return;}
+
+            selectedFileText.setText(fileSelectionStr.substring(0, fileSelectionStr.length() - 4));
+
+        });
+
+        //add everything to the scene root
+
+        introRoot.setAlignment(Pos.CENTER);
+        introRoot.setHgap(25);
+        introRoot.setVgap(15);
+
+        introRoot.add(title, 1,1);
+        introRoot.add(graphselectionPane, 1, 2);
+        introRoot.add(openButton, 1, 3);
+        introRoot.add(selectedFileText,1,4);
+
+        introRoot.add(startButton, 2,2);
+        introRoot.add(infoBoxButton, 3,2);
+
+        return new Scene(introRoot, WIDTH, HEIGHT);
+    }
+
+    private ScrollPane graphSelectionScrollPane() {
+
+        ScrollPane scrollPane = new ScrollPane();
+
+        //selectable list of options
+        ListView<String> listView = new ListView<String>();
+        listView.addEventHandler(MouseEvent.MOUSE_CLICKED, handleListClick(listView));
+
+        //read in file names into list for listview
+        File folder = new File("resources");
+        ObservableList<String> items = FXCollections.observableArrayList (Arrays.asList(folder.list()));
+
+
+        listView.setItems(items);
+
+        //prepare scroll pane
+        scrollPane.setContent(listView);
+        scrollPane.setMaxHeight(300);
+        scrollPane.setFitToWidth(true);
+        return scrollPane;
+    }
+
+    private EventHandler<MouseEvent> handleConfirm() {
+        return new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+
+                if (fileSelection != null){
+                    initGraph(fileSelection);
+                    window.setScene(new Scene(graphDisplay.getRoot(),WIDTH, HEIGHT));
+                } else {
+                    selectedFileText.setText("Please Choose File");
+                }
+            }
+        };
+    }
+
+    private EventHandler<MouseEvent> handleListClick(ListView<String> listView) {
+        return new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                fileSelectionStr = listView.getSelectionModel().getSelectedItem();
+
+                fileSelection = new File("resources/" + fileSelectionStr);
+                selectedFileText.setText(fileSelectionStr.substring(0, fileSelectionStr.length() - 4));
+
+            }
+        };
+    }
+
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
